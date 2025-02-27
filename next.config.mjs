167c5f6b03d339f2853f3ff19f04b6f8e700877a
ErinaldoJunior <erinaldogomesjr@gmail.com>
2025-02-27 21:50:38 +0000

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "api.dicebear.com",
-          port: "",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+const nextConfig = {};
+
+export default nextConfig;
